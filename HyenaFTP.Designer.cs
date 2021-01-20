616705d7e3bee9ce9b87f62d2f99bf1e3be78980
ellee <leeeunlin@icloud.com>
2021-01-20 17:32:38 +0900

    1차 push

---
+++ HyenaFTP.Designer.cs
@@ -0,0 +1,258 @@
+﻿
+namespace FTP
+{
+    partial class HyenaFTP
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtServer = new System.Windows.Forms.TextBox();
+            this.selectPath = new System.Windows.Forms.Button();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.backgroundWorker = new System.ComponentModel.BackgroundWorker();
+            this.IblStatus = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnUpload = new System.Windows.Forms.Button();
+            this.AllprogressBar = new System.Windows.Forms.ProgressBar();
+            this.AllbackgroundWorker = new System.ComponentModel.BackgroundWorker();
+            this.FilesinPro = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(87, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Server :";
+            // 
+            // txtServer
+            // 
+            this.txtServer.Location = new System.Drawing.Point(140, 41);
+            this.txtServer.Name = "txtServer";
+            this.txtServer.ReadOnly = true;
+            this.txtServer.Size = new System.Drawing.Size(256, 23);
+            this.txtServer.TabIndex = 1;
+            this.txtServer.Text = "192.10.10.227";
+            // 
+            // selectPath
+            // 
+            this.selectPath.Location = new System.Drawing.Point(407, 151);
+            this.selectPath.Name = "selectPath";
+            this.selectPath.Size = new System.Drawing.Size(75, 23);
+            this.selectPath.TabIndex = 4;
+            this.selectPath.Text = "Select Path";
+            this.selectPath.UseVisualStyleBackColor = true;
+            this.selectPath.Click += new System.EventHandler(this.selectPath_Click);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(140, 204);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(256, 12);
+            this.progressBar.TabIndex = 5;
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Location = new System.Drawing.Point(140, 77);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(256, 23);
+            this.txtUserName.TabIndex = 2;
+            this.txtUserName.Text = "test1";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(140, 114);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(256, 23);
+            this.txtPassword.TabIndex = 3;
+            this.txtPassword.Text = "1234";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(70, 117);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Password :";
+            // 
+            // backgroundWorker
+            // 
+            this.backgroundWorker.WorkerReportsProgress = true;
+            this.backgroundWorker.WorkerSupportsCancellation = true;
+            this.backgroundWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker_DoWork);
+            this.backgroundWorker.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker_ProgressChanged);
+            this.backgroundWorker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker_RunWorkerCompleted);
+            // 
+            // IblStatus
+            // 
+            this.IblStatus.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.IblStatus.Location = new System.Drawing.Point(0, 0);
+            this.IblStatus.Name = "IblStatus";
+            this.IblStatus.Size = new System.Drawing.Size(542, 24);
+            this.IblStatus.TabIndex = 7;
+            this.IblStatus.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(61, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 15);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "User Name :";
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(140, 152);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.ReadOnly = true;
+            this.txtPath.Size = new System.Drawing.Size(256, 23);
+            this.txtPath.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(96, 155);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 15);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Path :";
+            // 
+            // btnUpload
+            // 
+            this.btnUpload.Location = new System.Drawing.Point(407, 188);
+            this.btnUpload.Name = "btnUpload";
+            this.btnUpload.Size = new System.Drawing.Size(75, 23);
+            this.btnUpload.TabIndex = 11;
+            this.btnUpload.Text = "Upload";
+            this.btnUpload.UseVisualStyleBackColor = true;
+            this.btnUpload.Click += new System.EventHandler(this.btnUpload_Click);
+            // 
+            // AllprogressBar
+            // 
+            this.AllprogressBar.Location = new System.Drawing.Point(140, 188);
+            this.AllprogressBar.Name = "AllprogressBar";
+            this.AllprogressBar.Size = new System.Drawing.Size(256, 14);
+            this.AllprogressBar.TabIndex = 12;
+            // 
+            // AllbackgroundWorker
+            // 
+            this.AllbackgroundWorker.WorkerReportsProgress = true;
+            this.AllbackgroundWorker.WorkerSupportsCancellation = true;
+            this.AllbackgroundWorker.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.AllbackgroundWorker_ProgressChanged);
+            // 
+            // FilesinPro
+            // 
+            this.FilesinPro.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.FilesinPro.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.FilesinPro.Location = new System.Drawing.Point(0, 0);
+            this.FilesinPro.Name = "FilesinPro";
+            this.FilesinPro.Size = new System.Drawing.Size(542, 19);
+            this.FilesinPro.TabIndex = 13;
+            this.FilesinPro.Text = "Waiting...";
+            this.FilesinPro.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.FilesinPro);
+            this.panel1.Location = new System.Drawing.Point(12, 222);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(542, 19);
+            this.panel1.TabIndex = 15;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.IblStatus);
+            this.panel2.Location = new System.Drawing.Point(12, 244);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(542, 24);
+            this.panel2.TabIndex = 16;
+            // 
+            // HyenaFTP
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(572, 280);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.AllprogressBar);
+            this.Controls.Add(this.btnUpload);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.selectPath);
+            this.Controls.Add(this.txtUserName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtServer);
+            this.Controls.Add(this.label1);
+            this.Name = "HyenaFTP";
+            this.Text = "HyenaFTP";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtServer;
+        private System.Windows.Forms.Button selectPath;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label3;
+        private System.ComponentModel.BackgroundWorker backgroundWorker;
+        private System.Windows.Forms.Label IblStatus;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnUpload;
+        private System.Windows.Forms.ProgressBar AllprogressBar;
+        private System.ComponentModel.BackgroundWorker AllbackgroundWorker;
+        private System.Windows.Forms.Label FilesinPro;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
+
---
+++ HyenaFTP.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using FluentFTP;
+
+namespace FTP
+{
+    public partial class HyenaFTP : Form
+    {
+        public HyenaFTP()
+        {
+            InitializeComponent();
+        }
+
+
+        private void backgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            var folderPath = txtPath.Text;
+            using (var ftp = new FtpClient(txtServer.Text, txtUserName.Text, txtPassword.Text))
+            {
+                ftp.Connect();
+                Action<FtpProgress> pro = new Action<FtpProgress> (s=> 
+                {
+
+                    AllbackgroundWorker.ReportProgress((int)s.FileIndex + 1, (int)s.FileCount);
+
+                    backgroundWorker.ReportProgress((int)s.Progress, s.LocalPath);
+
+
+                });
+
+                ftp.UploadDirectory(folderPath, @"\", FtpFolderSyncMode.Update, FtpRemoteExists.Skip, FtpVerify.None, null, pro);
+
+            }
+        }
+        private void AllbackgroundWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+
+            FilesinPro.Text = $"{e.ProgressPercentage} of {e.UserState} files in progress";
+            AllprogressBar.Maximum = ((int)e.UserState);
+            AllprogressBar.Value = e.ProgressPercentage;
+            AllprogressBar.Update();
+        }
+        private void backgroundWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+
+            IblStatus.Text = $"{e.UserState} : {e.ProgressPercentage} %";
+            progressBar.Value = e.ProgressPercentage;
+            progressBar.Update();
+        }
+
+
+        private void backgroundWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            IblStatus.Text = "Upload Complete !";
+        }
+
+
+        private void selectPath_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                string[] files = Directory.GetFiles(fbd.SelectedPath);
+                txtPath.Text = fbd.SelectedPath;
+            }
+        }
+
+        private void btnUpload_Click(object sender, EventArgs e)
+        {
+            backgroundWorker.RunWorkerAsync();
+        }
+    }
+}
